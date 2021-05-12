8f58fdbfe9f7a6d785a51f8ccf0ec24f471c5a2b
Pedro Suarez <pfsuarez@gmail.com>
2021-05-12 14:40:27 +0200

    initial section code

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  console.log("", __dirname);
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
