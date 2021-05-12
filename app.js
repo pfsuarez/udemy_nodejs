1d168e09e4146058d00debb85c157fc2cae6ab94
Pedro Suarez <pfsuarez@gmail.com>
2021-05-12 10:59:29 +0200

    using express routing

--- app.js
+++ app.js
@@ -2,35 +2,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import adminRoutes from "./routes/admin.js";
+import shopRoutes from "./routes/shop.js";
+
 const app = express();
 
 app.use(bodyParser.urlencoded());
-
-// app.use("/users", (req, res, next) => {
-//   res.send("<h1>Users Page</h1>")
-// });
-
-// app.use("/", (req, res, next) => {
-//   res.send("<h1>Home Page</h1>")
-// });
-
-// app.listen(3000);
-
-app.use("/add-product", (req, res, next) => {
-  return res.send(`<form action="/product" method="POST">
-    <input type="text" name="title">
-    <button type="submit">Add Product</button>
-  </form>`)
-});
-
-app.post("/product", (req, res, next) => {
-  console.log("", req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  
-  return res.send("<h1>Hello from express.js</h1>")
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
