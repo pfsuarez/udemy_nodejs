22497673ebf425d3147dbc06681e37c7da811beb
Pedro Suarez <pfsuarez@gmail.com>
2021-05-12 10:36:39 +0200

    using express.js

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-// const http = require("http");
+// const express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
 
-import http from "http";
+const app = express();
 
-const server = http.createServer();
+app.use(bodyParser.urlencoded());
 
-server.listen(3000);
+// app.use("/users", (req, res, next) => {
+//   res.send("<h1>Users Page</h1>")
+// });
+
+// app.use("/", (req, res, next) => {
+//   res.send("<h1>Home Page</h1>")
+// });
+
+// app.listen(3000);
+
+app.use("/add-product", (req, res, next) => {
+  return res.send(`<form action="/product" method="POST">
+    <input type="text" name="title">
+    <button type="submit">Add Product</button>
+  </form>`)
+});
+
+app.post("/product", (req, res, next) => {
+  console.log("", req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  
+  return res.send("<h1>Hello from express.js</h1>")
+});
+
+app.listen(3000);
