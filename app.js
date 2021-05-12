2003426b5c1a08569bab4e94f1923a99aaea8633
Pedro Suarez <pfsuarez@gmail.com>
2021-05-12 12:04:17 +0200

    serving html and static files

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 // const express = require("express");
 import express from "express";
 import bodyParser from "body-parser";
+import path from "path";
 
+import { __dirname } from "./helper/helper.js";
 import adminRoutes from "./routes/admin.js";
 import shopRoutes from "./routes/shop.js";
+
 
 const app = express();
 
 app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>")
+  console.log("", __dirname);
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,12 @@
 import express from "express";
+import path from "path";
+
+import { __dirname } from "../helper/helper.js";
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  return res.send("<h1>Hello from express.js</h1>")
+  return res.sendFile(path.join(__dirname, "views" , "shop.html"));
 });
 
 export default router;
