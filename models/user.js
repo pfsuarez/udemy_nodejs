64b2ccd1e25cd565054069f623d17395a33b103d
Pedro Suarez <pfsuarez@gmail.com>
2021-05-21 15:01:43 +0200

    Storing the User in our Database

--- models/user.js
+++ models/user.js
@@ -4,13 +4,25 @@ import { getDb } from "../helper/database.js";
 const collectionName = "users";
 
 export class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart; // { items: [] }
+    this.id = new mongodb.ObjectId(id);
   }
 
   save() {
     return getDb().collection(collectionName).insertOne(this);
+  }
+
+  addToCart(product) {
+    //const cartProduct = this.cart.items.findIndex(cp => cp._id === product._id)
+
+    const updatedCart = { items: [{ ...product, qty: 1 }] };
+
+    return getDb()
+      .collection(collectionName)
+      .updateOne({ _id: this.id }, { $set: { cart: updatedCart } });
   }
 
   static findById(id) {
