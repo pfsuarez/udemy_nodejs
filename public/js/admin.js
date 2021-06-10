3a4712697d3abc3dc00dee47a35e8b0517892e7b
Pedro Suarez <pfsuarez@gmail.com>
2021-06-10 10:34:18 +0200

    Manipulating the DOM

--- public/js/admin.js
+++ public/js/admin.js
@@ -3,12 +3,18 @@ const deleteProduct = (btn) => {
   const productId = btn.parentNode.querySelector("[name=productId]").value;
   const csrf = btn.parentNode.querySelector("[name=_csrf]").value;
 
+  const productElement = btn.closest("article");
+
   fetch(`/admin/product/${productId}`, {
     method: "DELETE",
     headers: {
       "csrf-token": csrf,
     },
   })
-    .then(result => console.log(result))
+    .then(result => result.json())
+    .then(data => {
+      // productElement.parentNode.removeChild(productElement);
+      productElement.remove();
+    })
     .catch((err) => console.log(err));
 };
