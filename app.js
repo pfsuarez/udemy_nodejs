43d93636138d3469976f4f832d5aa6798bc08f19
Pedro Suarez <pfsuarez@gmail.com>
2021-06-21 11:32:53 +0200

    Connecting the Frontend to the GraphQL API

--- app.js
+++ app.js
@@ -50,6 +50,10 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*"); // domain.com
   res.setHeader("Access-Control-Allow-Methods", "*"); // GET, POST, PUT, PATCH, DELETE
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); //
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
   next();
 });
 
